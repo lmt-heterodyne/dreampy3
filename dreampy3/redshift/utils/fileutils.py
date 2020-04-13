ca2f5faca9adbf50bb8b789e9a441393502415f1
David Sanchez-Arguelles <domars@inaoep.mx>
2020-04-13 17:52:57 -0500

    Allow looking for the RSR data in a user defined directory via the DATA_LMT environment variable

--- dreampy3/redshift/utils/fileutils.py
+++ dreampy3/redshift/utils/fileutils.py
@@ -7,6 +7,15 @@ from .exceptions import LMTRedshiftError
 import glob
 import os
 
+def get_data_root():
+    if "DATA_LMT" in os.environ.keys():
+        data_root =os.environ["DATA_LMT"].strip()
+        if data_root[-1] !=os.sep:
+            data_root +=os.sep
+    else:
+        data_root="/data_lmt/"
+    return data_root
+
 def make_filename(date, scan, chassis, subobsnum=1):
     """
     makes a filename from date, scan number, and chassis number
@@ -21,19 +30,19 @@ def make_filename(date, scan, chassis, subobsnum=1):
         raise LMTRedshiftError("Arg Error", "Cannot parse date: %s" % date)
     if chassis not in (0, 1, 2, 3):
         raise LMTRedshiftError("Arg Error", "Chassis should be one of 0, 1, 2 or 3")
-    filename = '/data_lmt/RedshiftChassis%s/RedshiftChassis%s_%s_%06d_%02d_0001.nc' % (chassis, chassis, dt.strftime('%Y-%m-%d'), scan, subobsnum)
+    filename = get_data_root()+ 'RedshiftChassis%s/RedshiftChassis%s_%s_%06d_%02d_0001.nc' % (chassis, chassis, dt.strftime('%Y-%m-%d'), scan, subobsnum)
     return filename
 
 def make_generic_filename_old(scan, chassis, subobsnum=1):
     if subobsnum is None:
-        glb = glob.glob('/data_lmt/RedshiftChassis%s/RedshiftChassis%s_*_%06d_*_0001.nc' % (chassis, chassis, scan))
+        glb = glob.glob(get_data_root()+'RedshiftChassis%s/RedshiftChassis%s_*_%06d_*_0001.nc' % (chassis, chassis, scan))
     else:
-        glb = glob.glob('/data_lmt/RedshiftChassis%s/RedshiftChassis%s_*_%06d_%02d_0001.nc' % (chassis, chassis, scan, subobsnum))
+        glb = glob.glob(get_data_root()+'RedshiftChassis%s/RedshiftChassis%s_*_%06d_%02d_0001.nc' % (chassis, chassis, scan, subobsnum))
     if glb:
         return glb[0]
 
 def make_generic_filename(obsnum, chassis):
-    glb = glob.glob(os.path.join('/data_lmt', 'RedshiftChassis%s/RedshiftChassis%s*%06d*.nc' % (chassis, chassis, obsnum)))
+    glb = glob.glob(os.path.join(get_data_root(), 'RedshiftChassis%s/RedshiftChassis%s*%06d*.nc' % (chassis, chassis, obsnum)))
     if glb:
         return glb[0]
     
